f153e8e19f9d89c960035364ff2de57fda26ac10
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-08-20 00:03:38 +0500

    progrm run

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import React from "react";                  
-import React , {useEffect , useState} from "react";
+import React, { useEffect, useState } from "react";
 
-function App () {
+function App() {
+    const [data, setData] = useState([]);
 
-        const[data ,setdata ] = useState([]);
-    useEffect ( ()=>{
-        fetch( 'https://goweather.xyz/weather/taxila')
-        .then( response => response.json )
-        .then( data ,  setdata(data))
-        .catch( error=> console.error("data in fetching",error))
-    },[] )
+    useEffect(() => {
+        fetch('https://goweather.xyz/weather/wah')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error("Error in fetching data:", error));
+    }, []);
+
+    return (
+      <>
+      <div className="cards">
+        <h1 className="weather">Weather App</h1>
+
+        {
+          data.map((e ,index)=>{
+
+            <p key={index}>Temperature:{e.temperature}</p> 
+          })
+         }
+          </div>
+          
+      </>
+    );
 }
 
-const App = () => {
-  return (
-   
-    <>
-    {
-    data.map((temperature)=>(
-        <h1>"Temperature :"{temperature.wind}</h1>
-    ))
-    }
-    </>
-  )
-}
-
-export default App
-
+export default App;
