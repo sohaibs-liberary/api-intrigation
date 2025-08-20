e36805f400163d1658edb46bd57ba2090f05d9a3
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-08-20 15:07:04 +0500

    api fetch successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-    const [data, setData] = useState([]);
+  
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        fetch('https://goweather.xyz/weather/wah')
+  useEffect(() => {
+    fetch('https://goweather.xyz/weather/taxila')
             .then(response => response.json())
             .then(data => setData(data))
             .catch(error => console.error("Error in fetching data:", error));
-    }, []);
+          }, []);
+        
 
     return (
       <>
       <div className="cards">
         <h1 className="weather">Weather App</h1>
-
-        {
-          data.map((e ,index)=>{
-
-            <p key={index}>Temperature:{e.temperature}</p> 
-          })
-         }
+        <h1>temperature:{data.temperature}</h1>
+        <h1>wind:{data.wind}</h1>
           </div>
           
       </>
